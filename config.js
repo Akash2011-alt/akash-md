dfd56179b9f0c107a79e5065754812afa63d1268
Akash2011-alt <sathyajithakash459@gmail.com>
2026-07-18 12:02:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "xVdmWaoQ#nnO8ZMuqNdSO2GL78yMu7dorjt6iEnFEQiX00Czcz6M",
+SESSION_ID: process.env.SESSION_ID || "MR1SFSKY#oOfd6T_wz7sZMARru5aTD5dlnp6PUHAJP2GA-oQrzWY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/kawwagaming02/kawshala-md/blob/main/images/KAWSHALA-MD%20(1).jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 AKASH-MD Is Alive Now😍*",
 BOT_OWNER: '94764612317',  // Replace with the owner's phone number
